08e5119e8590206e3be474be0d46b337c0e319c1
keith <mrkeithhowell@gmail.com>
2017-12-21 15:12:43 +0000

    updaetd the read.bat with less spelling mistakes and changed a pause to be a timeout

--- read.bat
+++ read.bat
@@ -10,25 +10,25 @@ IF EXIST "%TempFile%" DEL "%TempFile%"
 IF EXIST "%OutFile%" DEL "%OutFile%"
 
 rem finds all lines with fatal at the start and puts them from in file to temp file 
-REM /n puts the line number in front 
+rem /n puts the line number in front 
 
 findstr /B /N Fatal   %InFile% > %tempfile%
 
-echo all fatals in temp file 
+echo all fatal in temp file 
 
 rem this gets rid of other errors 
 rem it only find words, a blank space work as breaks 
 rem /v find words that match and skip the lines that are in between the ""
-rem you can add /C. if you want to filter lines with two words 
+rem you can add /C. If you want to filter lines with two words 
 
 findstr /v /N /I " EDG.EDGE.Bluetooth.Services.MicrosoftBluetoothPortService.MicrosoftBluetoothPortStream.Write EDG.EDGE.Bluetooth.Services.MicrosoftBluetoothPortService.open()"    %tempfile% > %OutFile%
 
-echo finised after edit
+echo finished after edit
 
 SET quarantined=quarantined.txt
 IF EXIST "%quarantined%" DEL "%quarantined%"
 
-pause
+
 ECHO searching...
 FIND "quarantined" logfile.txt Logfile.1.txt 
 IF errorlevel 1 (
@@ -43,5 +43,5 @@ pause
 echo Start Time: %startTime%
 echo Finish Time: %time%
 ECHO all done 
-pause
+timeout 20
 
