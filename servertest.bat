fb6ce6739b2677aa4c568d7b09ca32a3e2f663d0
keith <mrkeithhowell@gmail.com>
2017-12-28 17:02:32 +0000

    added comments

--- servertest.bat
+++ servertest.bat
@@ -1,3 +1,6 @@
+REM Moving the old logfile 
+REM the server will create a new one 
+REM this is done so there is only one import per logfile
 MOVE D:\MobiliseMarketing\logs\logfile.txt D:\MobiliseMarketing\logs\logfile_beforeImport.txt
 (
 echo Import all files start %date% %time% 
@@ -5,12 +8,13 @@ echo ---------------------------------------------------
 ) >> D:\MobiliseMarketing\EDGSupport\kh\BatTesting\log.txt
 
 COPY D:\MobiliseMarketing\HostData\kh\*.DAT  D:\MobiliseMarketing\HostData
- 
+
+REM creates lock file
 type NUL > D:\MobiliseMarketing\HostData\lock.don
- 
+REM Calls import 
 D:\MobiliseMarketing\MobiliseServer\MobiliseServerClient Import tcp://localhost:9936 2223
 
-	
+REM could be removed as is won't do anything until import is done 
 	:loop
 		IF EXIST D:\MobiliseMarketing\HostData\lock.don (
 		timeout 120
@@ -19,7 +23,8 @@ D:\MobiliseMarketing\MobiliseServer\MobiliseServerClient Import tcp://localhost:
 echo Import all files finish %date% %time% 
 echo --------------------------------------------------- 
 ) >> D:\MobiliseMarketing\EDGSupport\kh\BatTesting\log.txt
- 
+
+REM TODO add summary in to the file rather then a;; fatal
 findstr /B /N Fatal  D:\MobiliseMarketing\logs\logfile.txt  >> D:\MobiliseMarketing\EDGSupport\kh\BatTesting\log.txt
 ECHO ----------------------DONE------------------------ >> D:\MobiliseMarketing\EDGSupport\kh\BatTesting\log.txt
 
