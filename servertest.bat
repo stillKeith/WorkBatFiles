f483b4a6fb66b5d2d46f7ff53555d3a40a2b3ec1
keith <mrkeithhowell@gmail.com>
2018-03-26 16:06:24 +0100

    updated the servertest file

--- servertest.bat
+++ servertest.bat
@@ -1,38 +1,38 @@
 REM Moving the old logfile 
 REM the server will create a new one 
 REM this is done so there is only one import per logfile
-MOVE D:\MobiliseMarketing\logs\logfile.txt D:\MobiliseMarketing\logs\logfile_beforeImport.txt
+MOVE D:\server\logs\logfile.txt D:\server\logs\logfile_beforeImport.txt
 (
 echo Import all files start %date% %time% 
 echo --------------------------------------------------- 
-) >> D:\MobiliseMarketing\EDGSupport\kh\BatTesting\log.txt
+) >> D:\server\Support\kh\BatTesting\log.txt
 
-COPY D:\MobiliseMarketing\HostData\kh\*.DAT  D:\MobiliseMarketing\HostData
+COPY D:\server\HostData\kh\*.DAT  D:\server\HostData
 
 REM creates lock file
-type NUL > D:\MobiliseMarketing\HostData\lock.don
+type NUL > D:\server\HostData\lock.don
 REM Calls import 
-D:\MobiliseMarketing\MobiliseServer\MobiliseServerClient Import tcp://localhost:9936 2223
+D:\server\MobiliseServer\serverLocation Import tcp://localhost:9936 2223
 
 REM could be removed as is won't do anything until import is done 
 	:loop
-		IF EXIST D:\MobiliseMarketing\HostData\lock.don (
+		IF EXIST D:\server\HostData\lock.don (
 		timeout 120
 		goto loop)
 ( 
 echo Import all files finish %date% %time% 
 echo --------------------------------------------------- 
-) >> D:\MobiliseMarketing\EDGSupport\kh\BatTesting\log.txt
+) >> D:\server\Support\kh\BatTesting\log.txt
 
 
-MOVE D:\MobiliseMarketing\logs\AllFatals.txt D:\MobiliseMarketing\logs\AllFatals_old.txt
-findstr /B /N Fatal  D:\MobiliseMarketing\logs\logfile.txt  > D:\MobiliseMarketing\EDGSupport\kh\BatTesting\AllFatals.txt
+MOVE D:\server\logs\AllFatals.txt D:\server\logs\AllFatals_old.txt
+findstr /B /N Fatal  D:\server\logs\logfile.txt  > D:\server\Support\kh\BatTesting\AllFatals.txt
 REM TODO add summary in to the file rather then all fatal
 echo Fatal errors in files:
 find /c "Fatal" log.txt
 echo -------------------------------------------------------------
 
-ECHO ----------------------DONE------------------------ >> D:\MobiliseMarketing\EDGSupport\kh\BatTesting\log.txt
+ECHO ----------------------DONE------------------------ >> D:\server\Support\kh\BatTesting\log.txt
 
 ECHO Done!
 Timeout 30
